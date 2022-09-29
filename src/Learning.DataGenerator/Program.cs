b7c50bdd4e5198e2bfcd6d71828b328f8ba5f978
Russell McDaniel <russell.mcdaniel@siliconesis.com>
2022-09-29 10:00:01 -0700

    Refactor the data generator utility (#23)

--- src/Learning.DataGenerator/Program.cs
+++ src/Learning.DataGenerator/Program.cs
@@ -7,6 +7,9 @@ using Learning.DataGenerator.Generators;
 using var host = Host.CreateDefaultBuilder(args)
     .ConfigureServices((context, services) =>
     {
+        services.Configure<GeneratorOptions>(
+            context.Configuration.GetSection("Generator"));
+
         services.AddSingleton<IEntityRepository, EntityRepository>();
         services.AddSingleton<IGenerator, Generator>();
         services.AddHostedService<GeneratorService>();
